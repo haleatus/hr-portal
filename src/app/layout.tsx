612e84f9e585f8d6d6188614034eb05cf726ed1b
RohitSth <shrestha.rohit655@gmail.com>
2025-03-06 16:52:35 +0545

    rm : removed the collapse button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/base/app-sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -39,12 +39,7 @@ export default function RootLayout({
               - Takes full available width
               - Maintains consistent padding
             */}
-          <main className="relative flex-1 w-full ">
-            <div className="absolute bottom-2 left-2">
-              <SidebarTrigger />
-            </div>
-            {children}
-          </main>
+          <main className="relative flex-1 w-full ">{children}</main>
 
           {/* Toast notifications */}
           <Toaster richColors position="bottom-left" />
